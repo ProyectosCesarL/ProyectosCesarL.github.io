1d05f479b7327dd095eb80cc35bb293215ae688d
ProyectosCesarL <126397914+ProyectosCesarL@users.noreply.github.com>
2023-03-14 16:31:14 -0300

    initial commit

---
+++ proyecto.js
@@ -0,0 +1,30 @@
+document.getElementById("miBoton").addEventListener("click",function(){
+	this.value="0353-154234567";
+});
+document.getElementById("miBoton").addEventListener("dblclick",function(){
+	this.value="Ver...";
+});
+document.getElementById("miBoton2").addEventListener("click",function(){
+	this.value="Corrientes 1450 Villa María (Córdoba)";
+});
+document.getElementById("miBoton2").addEventListener("dblclick",function(){
+	this.value="Ver...";
+});
+document.getElementById("miBoton3").addEventListener("click",function(){
+	this.value="alex_m@gmail.com";
+});
+document.getElementById("miBoton3").addEventListener("dblclick",function(){
+	this.value="Ver...";
+});
+document.getElementById("miBoton4").addEventListener("click",function(){
+	this.value="@alex_m";
+});
+document.getElementById("miBoton4").addEventListener("dblclick",function(){
+	this.value="Ver...";
+});
+document.getElementById("miBoton5").addEventListener("click",function(){
+	this.value="Alex Martinez";
+});
+document.getElementById("miBoton5").addEventListener("dblclick",function(){
+	this.value="Ver...";
+});
